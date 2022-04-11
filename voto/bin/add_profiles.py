78da3a1ea1678fd479340c8299ae309ce617855a
Callum Rollo <c.rollo@outlook.com>
2022-04-11 16:44:26 +0200

    get total mission profiles from raw input files

--- voto/bin/add_profiles.py
+++ voto/bin/add_profiles.py
@@ -8,8 +8,16 @@ def add_all_profiles():
     in_dir = Path("/home/callum/Documents/data-flow/nrt_data")
     ncs = list(in_dir.rglob("*gridfiles/*.nc"))
     for file in ncs:
+        rawncs = list(Path("/".join(file.parts[:-2]) + "/rawnc").glob("*.nc"))
+        dive_nums = []
+        for dive in rawncs:
+            try:
+                dive_nums.append(int(dive.name.split(".")[-2]))
+            except ValueError:
+                continue
+        max_profile = 2 * max(dive_nums)
         ds = xr.open_dataset(file)
-        add_glidermission(ds)
+        add_glidermission(ds, total_profiles=max_profile)
 
 
 if __name__ == "__main__":
