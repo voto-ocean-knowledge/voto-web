330d2dc7044fab81d93d5e4a2f45d10aa30930c8
Callum Rollo <c.rollo@outlook.com>
2022-04-11 16:15:51 +0200

    add basic home page test

---
+++ tests/_all_tests.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+container_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, container_folder)
+
+# noinspection PyUnresolvedReferences
+from home_tests import test_homepage_missions  # noqa
