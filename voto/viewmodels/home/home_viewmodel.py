106831242251db345facdb945bdc7e0354eeb727
Callum Rollo <c.rollo@outlook.com>
2022-04-14 14:17:33 +0200

    show current mission plots on home page

--- voto/viewmodels/home/home_viewmodel.py
+++ voto/viewmodels/home/home_viewmodel.py
@@ -1,6 +1,6 @@
 from voto.services.json_conversion import glidermission_to_json, blank_json_dict
 from voto.viewmodels.shared.viewmodelbase import ViewModelBase
-import voto.services.profile_service as profile_services
+import voto.services.profile_service as profile_service
 
 
 class IndexViewModel(ViewModelBase):
@@ -13,18 +13,29 @@ class IndexViewModel(ViewModelBase):
             self.profile_count,
             self.glider_count,
             self.total_time,
-        ) = profile_services.totals()
+        ) = profile_service.totals()
 
     def check_missions(self):
-        gliders, missions = profile_services.recent_glidermissions()
+        gliders, missions = profile_service.recent_glidermissions()
         glider_points_json = []
         glider_lines_json = []
         gliders_json = []
-        for glider, mission in zip(gliders, missions):
+        for i, (glider, mission) in enumerate(zip(gliders, missions)):
             point_json, line_json, glider_dict = glidermission_to_json(glider, mission)
             glider_points_json.append(point_json)
             glider_lines_json.append(line_json)
             gliders_json.append(glider_dict)
+            self.glidermission = profile_service.select_glidermission(glider, mission)
+            self.glider_fill = str(glider).zfill(3)
+            self.__setattr__(
+                f"combi_plot_{i}",
+                f"/static/img/glider/nrt/SEA{glider}/M{mission}/SEA{glider}_M{mission}.png",
+            )
+            self.__setattr__(
+                f"map_{i}",
+                f"/static/img/glider/nrt/SEA{glider}/M{mission}/SEA{glider}_M{mission}_map.png",
+            )
+
         self.glider_points = glider_points_json
         self.glider_lines = glider_lines_json
         self.gliders = gliders_json
--- voto/viewmodels/mission/mission_viewmodel.py
+++ voto/viewmodels/mission/mission_viewmodel.py
@@ -11,7 +11,6 @@ class MissionViewModel(ViewModelBase):
         self.glidermission = profile_service.select_glidermission(glider, mission)
         self.start_date = str(self.glidermission.start)[:10]
         self.end_date = str(self.glidermission.end)[:10]
-        self.glidermission.start_date = str(self.glidermission.start)[:10]
         self.combi_plot = (
             f"/static/img/glider/nrt/SEA{glider}/M{mission}/SEA{glider}_M{mission}.png"
         )
