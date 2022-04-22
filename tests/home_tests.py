2433095c6e1c2675f021308ad6c37618788ef50a
Callum Rollo <c.rollo@outlook.com>
2022-04-22 17:21:13 +0200

    glider tests

--- tests/home_tests.py
+++ tests/home_tests.py
@@ -1,7 +1,7 @@
 from flask import Response
 
 from tests.test_client import flask_app
-from voto.views import home_views
+from voto.views import home_views, mission_views, platform_views
 
 
 def test_homepage_missions():
@@ -13,13 +13,27 @@ def test_homepage_missions():
 
 def test_mission_list_page():
     with flask_app.test_request_context(path="/missions"):
-        r: Response = home_views.index()
+        r: Response = mission_views.mission_list()
     assert r.status_code == 200
     assert b"Missions" in r.data
 
 
 def test_mission_page():
     with flask_app.test_request_context(path="/SEA63/M33"):
-        r: Response = home_views.index()
+        r: Response = mission_views.missions(63, 33)
     assert r.status_code == 200
     assert b"mission" in r.data
+
+
+def test_platform_list_page():
+    with flask_app.test_request_context(path="/fleet"):
+        r: Response = platform_views.platform_list()
+    assert r.status_code == 200
+    assert b"Gliders" in r.data
+
+
+def test_glider_page():
+    with flask_app.test_request_context(path="fleet/SEA63"):
+        r: Response = platform_views.glider_page(63)
+    assert r.status_code == 200
+    assert b"glider" in r.data
