62cc41671dbac190177855cfa0a31f16c07e96c3
Callum Rollo <c.rollo@outlook.com>
2022-05-04 10:44:05 +0200

    add data to remote database

--- voto/bin/add_profiles.py
+++ voto/bin/add_profiles.py
@@ -2,16 +2,22 @@ import xarray as xr
 from pathlib import Path
 import logging
 import os
+import json
 from voto.data.db_session import initialise_database
 from voto.services.mission_service import add_glidermission
 from voto.services.platform_service import update_glider
 
-folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+_log = logging.getLogger(__name__)
+with open(folder + "/mongo_secrets.json") as json_file:
+    secrets = json.load(json_file)
 
 
 def add_nrt_profiles():
     in_dir = Path("/home/callum/Documents/data-flow/nrt_data")
+    _log.info(f"adding nrt profiles from {in_dir}")
     ncs = list(in_dir.rglob("*gridfiles/*.nc"))
+    _log.info(f"found {len(ncs)} files")
     for file in ncs:
         rawncs = list(Path("/".join(file.parts[:-2]) + "/rawnc").glob("*.nc"))
         dive_nums = []
@@ -24,6 +30,7 @@ def add_nrt_profiles():
         ds = xr.open_dataset(file)
         mission = add_glidermission(ds, total_profiles=max_profile)
         update_glider(mission)
+    _log.info("added all nrt profiles")
 
 
 def add_complete_profiles():
@@ -45,5 +52,10 @@ if __name__ == "__main__":
         level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    initialise_database()
+    initialise_database(
+        user=secrets["mongo_user"],
+        password=secrets["mongo_password"],
+        port=int(secrets["mongo_port"]),
+        server=secrets["mongo_server"],
+    )
     add_nrt_profiles()
