d0b182c688e9525e3d175ea91da980071c30fef7
Callum Rollo <c.rollo@outlook.com>
2022-05-11 10:48:35 +0200

    add mission list and stats to glider pages

--- voto/viewmodels/platform/platform_viewmodel.py
+++ voto/viewmodels/platform/platform_viewmodel.py
@@ -1,11 +1,11 @@
-from voto.data.db_classes import Glider
+from voto.data.db_classes import Glider, GliderMission
 from voto.services.utility_functions import seconds_to_pretty
 from voto.viewmodels.shared.viewmodelbase import ViewModelBase
 
 
 class PlatformListViewModel(ViewModelBase):
     def __init__(self):
-        gliders = Glider.objects()
+        gliders = Glider.objects().order_by("glider")
         for glider in gliders:
             glider.glider_fill = str(glider.glider).zfill(3)
             glider.pretty_time = seconds_to_pretty(glider.total_seconds)
@@ -22,3 +22,13 @@ class GliderViewModel(ViewModelBase):
         self.total_missions = len(self.glider.missions)
         self.pretty_time = seconds_to_pretty(self.glider.total_seconds)
         self.marianas = round(self.glider.total_depth / 21968, 1)
+        if self.marianas > 10:
+            self.marianas = int(self.marianas)
+        self.iss = round(self.glider.total_depth / (800 * 1000), 1)
+        glider_missions = GliderMission.objects(glider=int(self.glider_num))
+        for gm in glider_missions:
+            gm.glider_fill = str(gm.glider).zfill(3)
+            gm.start_pretty = str(gm.start)[:10]
+            gm.duration_pretty = (gm.end - gm.start).days
+            gm.variables_pretty = ", ".join(gm.variables)
+        self.glidermissions = glider_missions
