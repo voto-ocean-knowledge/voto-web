8f05a9d82c0e5640f6e652d7cdf6adf7febec8b0
Chiara Monforte <75482817+MOchiara@users.noreply.github.com>
2024-03-22 09:14:45 +0100

    Added new warnings for SB

--- voto/bin/add_sailbuoy.py
+++ voto/bin/add_sailbuoy.py
@@ -17,7 +17,7 @@ from voto.services.platform_service import update_sailbuoy
 from voto.services.geo_functions import get_seas
 from static_plots import sailbuoy_nrt_plots, make_map
 
-leak_mails = ["callum.rollo@voiceoftheocean.org"]
+leak_mails = ["callum.rollo@voiceoftheocean.org", "alarms-aaaak6sn7vydeww34wcbshfqdq@voice-of-the-ocean.slack.com" ]
 
 
 def all_nrt_sailbuoys(full_dir, all_missions=False):
@@ -114,17 +114,29 @@ def add_nrt_sailbuoy(df_in, sb, mission):
     _log.info(f"plotting sailbuoy data from SB{sb} mission {mission}")
     sailbuoy_nrt_plots(ds)
     make_map(ds)
-    if df_in.Leak.any() or df_in.BigLeak.any():
-        leak_alert_email(ds)
+    send_alert_email(ds)
     _log.info(f"Completed add SB{sb} mission {mission}")
 
-
-def leak_alert_email(ds_mission):
-    sb_num = ds_mission.attrs["sailbuoy_serial"]
-    msg = f"Leak detected in Sailbuoy {sb_num}"
-    mailer(msg, leak_mails)
-
-
+def send_alert_email(df_in):
+    sb_num = df_in.attrs["sailbuoy_serial"]
+    msg=[]
+    subset_data = df_in[-15:]
+    if df_in.Leak[-15:].any() or df_in.BigLeak.Leak[-15:].any():
+        msg_l = f"Leak detected in Sailbuoy {sb_num}"
+    if len(df_in.Leak[-15:].unique())==1 or len(df_in.BigLeak.Leak[-15:].unique()) == 1:
+        msg_l = str()
+    if df_in.Warning[-15:].any():
+        msg_w = f"There is a warning for Sailbuoy {sb_num}"
+    if len(df_in.Warning[-15:].unique())==1:
+        msg_w = str()
+    if not df_in.WithinTrackRadius[-15:].any():
+        msg_t = f"The Sailbuoy {sb_num} is off track"
+    if len(df_in.WithinTrackRadius[-15:].unique())==1:
+        msg_w = str()
+    msg = "\n".join([msg_l, msg_w,msg_t])
+    if len(msg) > 2:
+        mailer(msg, leak_mails)
+        
 def mailer(message, recipients):
     for recipient in recipients:
         subprocess.check_call(
