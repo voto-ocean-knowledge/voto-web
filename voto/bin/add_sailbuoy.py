08ef7cea7a27fec3aacd1617d75dc33e228ecf11
Callum Rollo <c.rollo@outlook.com>
2024-04-26 09:10:43 +0200

    error catch on reading sb files

--- voto/bin/add_sailbuoy.py
+++ voto/bin/add_sailbuoy.py
@@ -66,8 +66,12 @@ def split_nrt_sailbuoy(
     min_mission_time=datetime.timedelta(days=3),
     mission_num=1,
 ):
-    df_nav = pd.read_csv(nav, sep="\t", parse_dates=["Time"])
-    df_pld = pd.read_csv(pld, sep="\t", parse_dates=["Time"])
+    try:
+        df_nav = pd.read_csv(nav, sep="\t", parse_dates=["Time"])
+        df_pld = pd.read_csv(pld, sep="\t", parse_dates=["Time"])
+    except pd.errors.ParserError:
+        _log.error(f"could not read one of {nav}, {pld}")
+        return mission_num
     if len(df_nav) == 0 or len(df_pld) == 0:
         return mission_num
     df_combi = pd.merge_asof(
